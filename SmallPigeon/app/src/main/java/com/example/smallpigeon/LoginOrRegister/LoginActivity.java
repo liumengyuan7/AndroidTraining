ad64e3845894609ebbca7d48d24c5741a32fb361
sunyufei666 <156267102@qq.com>
2019-11-25 14:09:43 +0800

    androidx
    androidx

--- SmallPigeon/app/src/main/java/com/example/smallpigeon/LoginOrRegister/LoginActivity.java
+++ SmallPigeon/app/src/main/java/com/example/smallpigeon/LoginOrRegister/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
