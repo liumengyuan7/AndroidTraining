c7d9642834034bc46da8742aefc36de682bb32d9
sunyufei666 <156267102@qq.com>
2019-11-25 14:16:13 +0800

    1
    1

--- Server/SmallPigeon/src/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package user.service;
-
-import user.dao.UserDao;
-
-public class UserService {
-	public boolean userLogin(String username,String password) {
-		return new UserDao().userLogin(username, password);
-	}
-}
