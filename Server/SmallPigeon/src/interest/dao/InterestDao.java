bacbb4b9bcb23fc32811374e1d5100695433f40d
sunyufei666 <156267102@qq.com>
2019-11-27 14:49:32 +0800

    1911271449
    后台的优化

--- Server/SmallPigeon/src/interest/dao/InterestDao.java
+++ Server/SmallPigeon/src/interest/dao/InterestDao.java
@@ -13,11 +13,6 @@ public class InterestDao {
     public boolean insertInterest(String interest,String userId){
         String[] single = interest.split(",");
         new Interest().set("user_id",Integer.parseInt(userId));
-//        new Interest().set("user_id",Integer.parseInt(userId));
-//        for(int i = 0;i<single.length;i++){
-////            new Interest().set(single[i],1);
-//            System.out.println(single[i]);
-//        }
         return false;
     }
 
--- Server/SmallPigeon/src/user/dao/UserDao.java
+++ Server/SmallPigeon/src/user/dao/UserDao.java
@@ -2,13 +2,25 @@ package user.dao;
 
 import com.google.gson.Gson;
 
+import java.io.UnsupportedEncodingException;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import bean.Interest;
 import bean.User;
-import interest.service.InterestService;
 
 public class UserDao {
 
@@ -45,27 +57,60 @@ public class UserDao {
 	public String userRegister(String userEmail,String userPassword,String userNickname,String userSex,String userInterest){
 		List<User> confirm = new User().dao().find("select * from user where user_email=?",userEmail);
 		if(confirm.isEmpty()){
-			Date date = new Date();
-			Timestamp timestamp = new Timestamp(date.getTime());
-			boolean result = new User().set("user_email",userEmail)
-					.set("user_password",userPassword).set("user_nickname",userNickname)
-					.set("user_register_time",timestamp).set("user_sex",userSex).save();
-
-			String userId = new User().dao().find("select id from user where user_email=?",userEmail).get(0).getStr("id");
-			new Interest().set("user_id",userId).save();
-			String[] in = userInterest.split(",");
-			Interest interest = Interest.dao.findById(new Interest().dao().find("select id from interest where user_id=?",userId).get(0).getStr("id"));
-			for(int i=0;i<in.length;i++){
-				interest.set(in[i],1);
-			}
-			interest.update();
-			if(result){
-				return "true";
-			}else{
-				return "false";
-			}
+			return dataBaseOperation(userEmail,userPassword,userNickname,userSex,userInterest);
 		}else{
 			return "repeat";
+		}
+	}
+
+	//注册中数据库的操作
+	public String dataBaseOperation(String userEmail,String userPassword,String userNickname,String userSex,String userInterest){
+		Date date = new Date();
+		Timestamp timestamp = new Timestamp(date.getTime());
+		boolean result = new User().set("user_email",userEmail)
+				.set("user_password",userPassword).set("user_nickname",userNickname)
+				.set("user_register_time",timestamp).set("user_sex",userSex).save();
+		String userId = new User().dao().find("select id from user where user_email=?",userEmail).get(0).getStr("id");
+		new Interest().set("user_id",userId).save();
+		String[] in = userInterest.split(",");
+		Interest interest = Interest.dao.findById(new Interest().dao().find("select id from interest where user_id=?",userId).get(0).getStr("id"));
+		for(int i=0;i<in.length;i++){
+			interest.set(in[i],1);
+		}
+		interest.update();
+		if(result){
+			return "true";
+		}else{
+			return "false";
+		}
+	}
+
+	//邮件发送
+	public void emailSend(String userEmail,String code){
+		Properties props = System.getProperties();
+		props.put("mail.smtp.host", "smtp.163.com");
+		props.put("mail.smtp.auth", "true");
+		Session session = Session.getInstance(props, new Authenticator() {
+			public PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("qq2642611193@163.com", "qq1436630964");
+			}
+		});
+		Message msg = new MimeMessage(session);
+		try {
+			msg.setFrom(new InternetAddress("qq2642611193@163.com","小鸽快跑","UTF-8"));
+			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(userEmail));
+			msg.setSubject("为保证您的账号安全请验证邮箱");
+			MimeBodyPart mbp1 = new MimeBodyPart();
+			mbp1.setContent("<h3>【小鸽快跑】</h3><p>您的验证码是"+code+",您正在进行邮箱验证注册,5分钟内有效。(请勿向任何人提供您收到的验证码)</p>","text/html;charset=UTF-8");
+			MimeMultipart mimeMultipart = new MimeMultipart();
+			mimeMultipart.addBodyPart(mbp1);
+			msg.setContent(mimeMultipart);
+			msg.setHeader("X-Mailer", "smtpsend");
+			Transport.send(msg);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
 		}
 	}
 
