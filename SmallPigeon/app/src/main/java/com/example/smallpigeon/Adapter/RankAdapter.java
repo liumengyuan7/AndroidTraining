278e02bf968fbf9e279faff8c2b0c0ef89c60b24
xcy999 <45208943@qq.com>
2019-12-03 16:33:01 +0800

    12031632xcy
    1

---
+++ SmallPigeon/app/src/main/java/com/example/smallpigeon/Adapter/RankAdapter.java
@@ -0,0 +1,76 @@
+package com.example.smallpigeon.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.smallpigeon.R;
+
+import java.util.List;
+import java.util.Map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RankAdapter extends BaseAdapter{
+    private AppCompatActivity context;
+    private List<Map<String,Object>> dataSourse;
+    private int stringId;
+
+    public RankAdapter(AppCompatActivity context,List<Map<String,Object>> dataSourse,int id){
+        this.context = context;
+        this.dataSourse = dataSourse;
+        this.stringId = id;
+    }
+
+    @Override
+    public int getCount() {
+        return dataSourse.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return dataSourse.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (null == convertView) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            convertView = inflater.inflate(stringId, null);
+        }
+        //nickname
+        TextView userName = convertView.findViewById(R.id.gradeRank_userName);
+        userName.setText(dataSourse.get(position).get("userName").toString());
+       // 积分
+        TextView userPoints = convertView.findViewById(R.id.gradeRank_userPoints);
+        userPoints.setText(dataSourse.get(position).get("userPoints").toString());
+        //
+        String rank=dataSourse.get(position).get("rank").toString();
+        ImageView img= convertView.findViewById(R.id.user_ranking);
+
+        switch (rank){
+            case "1":
+                img.setImageDrawable(context.getResources().getDrawable(R.drawable.frist));
+                break;
+            case "2":
+                img.setImageDrawable(context.getResources().getDrawable(R.drawable.second));
+                break;
+            case "3":
+                img.setImageDrawable(context.getResources().getDrawable(R.drawable.thrid));
+                break;
+        }
+
+
+        notifyDataSetChanged();
+
+        return convertView;
+    }
+}
---
+++ SmallPigeon/app/src/main/java/com/example/smallpigeon/My/Grade_Rank.java
@@ -0,0 +1,118 @@
+package com.example.smallpigeon.My;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.smallpigeon.Adapter.RankAdapter;
+import com.example.smallpigeon.Fragment.RunFragment;
+import com.example.smallpigeon.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Grade_Rank extends AppCompatActivity {
+    private Button goRun;
+
+    List<Map<String,Object>> information;
+    RankAdapter customAdapter1;
+    String[] result1;
+    String[] result2;
+
+    private  Handler handler=new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String info = (String) msg.obj;
+            if(!info.equals("false")) {
+                result1 = info.split(";");
+                int a=1;
+                for (int i = 0; i < result1.length; i++) {
+                    Map<String, Object> itemData = new HashMap<>();
+                    result2 = result1[i].split(",");
+                    itemData.put("userName", result2[0]);
+                    itemData.put("userPoints", result2[1]);
+                    itemData.put("rank",a);
+                    information.add(itemData);
+                    a++;
+                    customAdapter1.notifyDataSetChanged();
+                }
+            }
+            else {
+                Toast toastTip = Toast.makeText(getApplicationContext(),"获取排行榜失败！请检查网络！",Toast.LENGTH_LONG);
+                toastTip.setGravity(Gravity.CENTER, 0, 0);
+                toastTip.show();
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grade__rank);
+        findViews();
+
+        information = new ArrayList<>();
+        ListView listView1 = findViewById(R.id.rank_points);
+        customAdapter1 = new RankAdapter(this,information,R.layout.grade_rank_listitem);
+        listView1.setAdapter(customAdapter1);
+
+        getPoints();
+
+        //点击按钮进入跑步界面
+        goRun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Grade_Rank.this, RunFragment.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    //向后台获取分数
+    public void getPoints(){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://"+getResources().getString(R.string.ip_address)
+                            +":8080/smallpigeon/user/gradeRank");
+                    URLConnection conn = url.openConnection();
+                    InputStream in = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in, "utf-8"));
+                    String result = reader.readLine();
+                    Message message = new Message();
+                    message.obj = result;
+                    handler.sendMessage(message);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+
+    public void findViews(){
+        goRun=findViewById(R.id.rank_GoRun);
+
+    }
+}
