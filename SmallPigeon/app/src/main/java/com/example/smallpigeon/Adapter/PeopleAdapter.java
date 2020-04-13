e9a8a503a3d678fbee9aa680c80f2388e2744c59
程璐 <695625950@qq.com>
2020-04-13 16:13:49 +0800

    评论界面

--- SmallPigeon/app/src/main/java/com/example/smallpigeon/Adapter/PeopleAdapter.java
+++ SmallPigeon/app/src/main/java/com/example/smallpigeon/Adapter/PeopleAdapter.java
@@ -1,11 +1,18 @@
 package com.example.smallpigeon.Adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.example.smallpigeon.Entity.DynamicContent;
@@ -17,10 +24,14 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import androidx.appcompat.app.ActionBar;
+
 public class PeopleAdapter extends BaseAdapter {
     private Context context;
     private int itemLayoutID;
     private List<DynamicContent> list = new ArrayList<>();
+
+    private PopupWindow mPopWindow;
 
     public PeopleAdapter(Context context, int itemLayoutID, List<DynamicContent> list) {
         this.context = context;
@@ -50,6 +61,9 @@ public class PeopleAdapter extends BaseAdapter {
             holder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(itemLayoutID, null);
+            holder.ll_forward = convertView.findViewById( R.id.ll_forward );
+            holder.ll_toComment = convertView.findViewById( R.id.ll_toComment );
+            holder.ll_like = convertView.findViewById( R.id.ll_like );
             holder.iv_unfold = convertView.findViewById(R.id.iv_unfold);
             holder.iv_comment = convertView.findViewById(R.id.iv_comment);
             holder.iv_icon = convertView.findViewById(R.id.iv_icon);
@@ -69,18 +83,62 @@ public class PeopleAdapter extends BaseAdapter {
         holder.device.setText(dynamicContent.getDevice());
         holder.dynamic_item_txt.setText(dynamicContent.getContent());
 
+        Log.e( "erro1", "error?" );
+        holder.ll_toComment.setOnClickListener( v -> new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e( "erro2", "error?" );
+                showPopupWindow();
+            }
+        } );
+
         return convertView;
     }
 
+    @SuppressLint("WrongConstant")
+    private void showPopupWindow() {
+        //设置contentView
+        View contentView = LayoutInflater.from(context).inflate(R.layout.comment_popuwindow, null);
+        mPopWindow = new PopupWindow(contentView,
+                ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT, true);
+        mPopWindow.setContentView(contentView);
+        //防止PopupWindow被软件盘挡住（可能只要下面一句，可能需要这两句）
+        mPopWindow.setSoftInputMode(PopupWindow.INPUT_METHOD_NEEDED);
+        mPopWindow.setSoftInputMode( WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        //设置软键盘弹出
+        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(1000, InputMethodManager.HIDE_NOT_ALWAYS);//这里给它设置了弹出的时间
+        //设置各个控件的点击响应
+//        final EditText editText = contentView.findViewById(R.id.pop_editText);
+//        Button btn = contentView.findViewById(R.id.pop_btn);
+
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String inputString = editText.getText().toString();
+//                Toast.makeText( getContext(), inputString, Toast.LENGTH_SHORT).show();
+//                mPopWindow.dismiss();//让PopupWindow消失
+//            }
+//        });
+        //是否具有获取焦点的能力
+        mPopWindow.setFocusable(true);
+        //显示PopupWindow
+        View rootview = LayoutInflater.from(context).inflate(R.layout.activity_main, null);
+        mPopWindow.showAtLocation(rootview, Gravity.BOTTOM, 0, 0);
+    }
+
     static class ViewHolder{
-         ImageView iv_icon;//用户头像
-         ImageView dynamic_item_img;//发表内容配图
-         ImageView iv_unfold;//转发
-         ImageView iv_comment;//评论
-         ImageView iv_praise;//赞
-         TextView tv_nickName;//用户昵称
-         TextView tv_date;//发表时间
-         TextView device;//设备名称
-         TextView dynamic_item_txt;//发表内容
+        ImageView iv_icon;//用户头像
+        ImageView dynamic_item_img;//发表内容配图
+        LinearLayout ll_forward;//转发
+        ImageView iv_unfold;
+        LinearLayout ll_toComment;//评论
+        ImageView iv_comment;
+        LinearLayout ll_like;//赞
+        ImageView iv_praise;//赞
+        TextView tv_nickName;//用户昵称
+        TextView tv_date;//发表时间
+        TextView device;//设备名称
+        TextView dynamic_item_txt;//发表内容
     }
 }
--- SmallPigeon/app/src/main/java/com/example/smallpigeon/Fragment/PeopleFragment.java
+++ SmallPigeon/app/src/main/java/com/example/smallpigeon/Fragment/PeopleFragment.java
@@ -1,23 +1,37 @@
 package com.example.smallpigeon.Fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smallpigeon.Adapter.PeopleAdapter;
 import com.example.smallpigeon.Entity.DynamicContent;
 import com.example.smallpigeon.Entity.UserContent;
 import com.example.smallpigeon.Fragment.Dynamic.ReleaseDynamic;
+import com.example.smallpigeon.MainActivity;
 import com.example.smallpigeon.R;
 
 import java.text.SimpleDateFormat;
@@ -30,6 +44,10 @@ public class PeopleFragment extends Fragment {
     private ListView dynamic_list;
     private ImageView iv_add_Message;//发表动态，右上角加号
 
+    private LinearLayout ll_toComment;
+
+    private PopupWindow mPopWindow;
+
     private MyClickListener listener;
     private PeopleAdapter adapter;
     private List<DynamicContent> list = new ArrayList<>();
@@ -37,8 +55,8 @@ public class PeopleFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_people,container,false);
-        getViews(view);
+        View view = inflater.inflate( R.layout.fragment_people, container, false );
+        getViews( view );
         listener = new MyClickListener();
         registerListener();
 
@@ -46,46 +64,50 @@ public class PeopleFragment extends Fragment {
 
         DynamicContent content = new DynamicContent();
         UserContent userContent = new UserContent();
-        userContent.setUserNickname("啦啦啦");
-        content.setDate(new SimpleDateFormat("yyyy年-MM月-dd日").format(new Date()));
-        content.setUserContent(userContent);
-        content.setContent("今日跑步分享");
-        content.setDevice(Build.MODEL);
-        list.add(content);
-        adapter = new PeopleAdapter(getContext(),R.layout.people_dynamic_listitem,list);
-        dynamic_list.setAdapter(adapter);
+        userContent.setUserNickname( "啦啦啦" );
+        content.setDate( new SimpleDateFormat( "yyyy年-MM月-dd日" ).format( new Date() ) );
+        content.setUserContent( userContent );
+        content.setContent( "今日跑步分享" );
+        content.setDevice( Build.MODEL );
+        list.add( content );
+        adapter = new PeopleAdapter( getContext(), R.layout.people_dynamic_listitem, list );
+        dynamic_list.setAdapter( adapter );
 
         return view;
     }
 
     private void registerListener() {
-        iv_add_Message.setOnClickListener(listener);
+        iv_add_Message.setOnClickListener( listener );
+        ll_toComment.setOnClickListener( listener );
     }
 
     private void getViews(View view) {
-        dynamic_list = view.findViewById(R.id.dynamic_list);
-        iv_add_Message = view.findViewById(R.id.iv_add_Message);
+        dynamic_list = view.findViewById( R.id.dynamic_list );
+        iv_add_Message = view.findViewById( R.id.iv_add_Message );
+        ll_toComment = view.findViewById( R.id.ll_toComment );
+
     }
 
-    class MyClickListener implements View.OnClickListener{
+    class MyClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            switch (v.getId()){
+            switch (v.getId()) {
                 case R.id.iv_add_Message:
                     //TODO:添加新的动态
                     //TODO:如果已登录，跳转发表动态
 //                    if (loginOrNot()){
-                        Intent intent = new Intent(getContext(),ReleaseDynamic.class);
-                        startActivity(intent);
+                    Intent intent = new Intent( getContext(), ReleaseDynamic.class );
+                    startActivity( intent );
 //                    } else {
 //                        Toast.makeText(getContext(),"请先登录哦！",Toast.LENGTH_SHORT).show();
 //                    }
                     break;
-                case R.id.iv_unfold:
+                case R.id.iv_comment:
                     //TODO:点击转发时可以转发
                     break;
-                case R.id.iv_comment:
+                case R.id.ll_toComment:
                     //TODO：点击评论可以进行评论
+                    showPopupWindow();
                     break;
                 case R.id.iv_praise:
                     //TODO:赞
@@ -94,5 +116,37 @@ public class PeopleFragment extends Fragment {
 
             }
         }
+    }
+
+    @SuppressLint("WrongConstant")
+    private void showPopupWindow() {
+        //设置contentView
+        View contentView = LayoutInflater.from(getContext()).inflate(R.layout.comment_popuwindow, null);
+        mPopWindow = new PopupWindow(contentView,
+                ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT, true);
+        mPopWindow.setContentView(contentView);
+        //防止PopupWindow被软件盘挡住（可能只要下面一句，可能需要这两句）
+        mPopWindow.setSoftInputMode(PopupWindow.INPUT_METHOD_NEEDED);
+        mPopWindow.setSoftInputMode( WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        //设置软键盘弹出
+        InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(1000, InputMethodManager.HIDE_NOT_ALWAYS);//这里给它设置了弹出的时间
+        //设置各个控件的点击响应
+//        final EditText editText = contentView.findViewById(R.id.pop_editText);
+//        Button btn = contentView.findViewById(R.id.pop_btn);
+
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String inputString = editText.getText().toString();
+//                Toast.makeText( getContext(), inputString, Toast.LENGTH_SHORT).show();
+//                mPopWindow.dismiss();//让PopupWindow消失
+//            }
+//        });
+        //是否具有获取焦点的能力
+        mPopWindow.setFocusable(true);
+        //显示PopupWindow
+        View rootview = LayoutInflater.from(getContext()).inflate(R.layout.activity_main, null);
+        mPopWindow.showAtLocation(rootview, Gravity.BOTTOM, 0, 0);
     }
 }
