2f754658eb3105736232385763e1ae7e3e8a1494
liumengyuan7 <904569030.qq.com>
2020-05-11 18:44:05 +0800

    我的收藏后端

---
+++ Server/SmallPigeon/src/com/community/dao/CollectMapper.java
@@ -0,0 +1,20 @@
+package com.community.dao;
+
+import com.entity.Dynamics;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface CollectMapper {
+    //我的所有收藏
+    public List<Dynamics> queryAllCollectByUserId(@Param("userId") String userId);
+    //得到收藏动态的评论数量
+    public int getCommentNum(@Param("dynamicId") int dynamicId);
+    //得到收藏动态的收藏数量
+    public int getCollectNum(@Param("dynamicId") int dynamicId);
+    //收藏
+    public int insertCollect(@Param("dynamicId") String dynamicId,@Param("userId") String userId);
+    //取消收藏
+    public int deleteCollect(@Param("dynamicId") String dynamicId,@Param("userId") String userId);
+}
